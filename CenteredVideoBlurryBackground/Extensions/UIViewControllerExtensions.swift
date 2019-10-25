709525401bbc235f7660a23d352530a8e7a690f6
Alan <alanpridestar@gmail.com>
2019-10-25 16:45:00 +0300

    first commit

---
+++ CenteredVideoBlurryBackground/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtensions.swift
+//  CenteredVideoBlurryBackground
+//
+//  Created by macmini7 on 10/24/19.
+//  Copyright © 2019 macmini7. All rights reserved.
+//
+
+import Foundation
